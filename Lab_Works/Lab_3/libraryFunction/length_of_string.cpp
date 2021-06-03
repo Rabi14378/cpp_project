5dc465ecd976d52db1e35664d7a2ab2b14a05be9
Rabi14378 <rabibro14378@gmail.com>
2021-06-03 19:49:43 +0545

    Print length of string

--- Lab_Works/Lab_3/libraryFunction/length_of_string.cpp
+++ Lab_Works/Lab_3/libraryFunction/length_of_string.cpp
@@ -4,8 +4,13 @@ using namespace std;
 
 int main(){
   string str;
+  char str2[20];
   cout<<"Enter any string"<<endl;
   getline(cin,str);
   cout<<"Length of string is :"<<str.length()<<endl;
+  
+  cout<<"Enter another string"<<endl;
+  cin>>str2;
+  cout<<"Length of string is :"<<strlen(str2)<<endl;
   return 0;
 }
