66b8475c9adbb18a7443bc097b7f75790121d644
Rabi14378 <rabibro14378@gmail.com>
2021-06-08 16:15:12 +0545

    Swaping by passing reference as argument

---
+++ Lab_Works/Lab_3/FunctionPass/pass_by_reference.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void swap(int &a ,int &b){
+  int temp;
+  temp=a;
+  a=b;
+  b=temp;
+}
+
+int main(){
+  int a,b;
+  cout<<"Enter two numbers";
+  cin>>a>>b;
+  cout<<"Value before swapping"<<endl<<"a ="<<a<<endl<<"b ="<<b<<endl;
+  swap(a,b);
+  cout<<"Value after swapping"<<endl<<"a ="<<a<<endl<<"b ="<<b<<endl;
+  return 0;
+}
