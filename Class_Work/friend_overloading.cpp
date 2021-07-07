a9167efe8aa8d295c6a2bd1355ab9fe1ab7b8d91
Rabi14378 <rabibro14378@gmail.com>
2021-07-07 13:44:39 +0545

    overloading operator to add distance of two object by frienf function

---
+++ Class_Work/friend_overloading.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class Distance {
+
+    int feet;
+    int inch;
+  public:
+    Distance();
+    Distance(int, int);
+    friend Distance operator+(Distance &,Distance &);
+    void display();
+    ~Distance(){}
+
+};
+
+Distance::Distance(){
+  this->feet=0;
+  this->inch=0;
+}
+
+Distance::Distance(int feet, int inch){
+  this->feet=feet;
+  this->inch=inch;
+}
+
+Distance operator+(Distance &d1,Distance &d2){
+  Distance temp;
+  temp.feet=d1.feet+d2.feet;
+  temp.inch=d1.inch+d2.inch;
+  return temp;
+}
+
+void Distance::display(){
+  cout<<"Total distance is: "<<feet<<" feet "<<inch<<" inch" <<endl;
+}
+
+int main(){
+  Distance d1(10,3),d2(2,4),d3;
+  d3=d1+d2;
+  d3.display();
+  return 0;
+}
