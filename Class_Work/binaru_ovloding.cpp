627910f6ebc19cda4db1aafe606682ccfe28ca83
Rabi14378 <rabibro14378@gmail.com>
2021-07-07 13:42:39 +0545

    overloading operator to add distance of two object

---
+++ Class_Work/binaru_ovloding.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class Distance {
+
+    int feet;
+    int inch;
+  public:
+    Distance();
+    Distance(int, int);
+    Distance operator+(Distance d1);
+    void display();
+    ~Distance(){}
+
+};
+
+Distance::Distance(){
+  this->feet=0;
+  this->inch=0;
+}
+
+Distance::Distance(int feet, int inch){
+  this->feet=feet;
+  this->inch=inch;
+}
+
+Distance Distance::operator+(Distance d1){
+  Distance temp;
+  temp.feet=this->feet+d1.feet;
+  temp.inch=this->inch+d1.inch;
+  return temp;
+}
+
+void Distance::display(){
+  cout<<"Total distance is: "<<feet<<" feet "<<inch<<" inch" <<endl;
+}
+
+int main(){
+  Distance d1(10,3),d2(2,4),d3;
+  d3=d1+d2;
+  d3.display();
+  return 0;
+}
